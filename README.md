5921e623b0d7b8d4b2d67c9b61af11f4a3ca0e55
João Gouveia <joaotalonegouveia@tecnico.ulisboa.pt>
2024-05-29 13:09:19 +0100

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# MankalaEngine
+
+MankalaEngine is an engine used for creating computerized opponents for Mancala variants.
---
+++ test/mankalaengine_test.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+
+TEST(MankalaEngine, Test1) {
+  EXPECT_EQ(7 * 6, 42);
+}
+
+TEST(MankalaEngine, Test2) {
+  EXPECT_EQ(7 * 6, 42);
+}
