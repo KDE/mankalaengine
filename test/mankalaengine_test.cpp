066c6d117b419458b32f0f21f730b1483bf7bf7b
João Gouveia <joaotalonegouveia@tecnico.ulisboa.pt>
2024-06-05 17:24:04 +0100

    feat: add mankala engine tests

--- test/mankalaengine_test.cpp
+++ test/mankalaengine_test.cpp
@@ -1,9 +1,29 @@
+#include <bohnenspielrules.h>
 #include <gtest/gtest.h>
+#include <mankalaengine.h>
+#include <moveselection.h>
 
-TEST(MankalaEngine, Test1) {
-  EXPECT_EQ(7 * 6, 42);
-}
+TEST(MankalaEngine, RandomSelectionFullGame) {
+    MankalaEngine::MankalaEngine engine(MankalaEngine::random);
+    MankalaEngine::BohnenspielBoard board;
+    MankalaEngine::BohnenspielRules rules;
+    MankalaEngine::Player p1 = MankalaEngine::first_player;
+    MankalaEngine::Player p2 = MankalaEngine::second_player;
+    bool p1_can_play = true;
+    bool p2_can_play = true;
 
-TEST(MankalaEngine, Test2) {
-  EXPECT_EQ(7 * 6, 42);
+    while (p1_can_play && p2_can_play) {
+        p1_can_play = engine.play(rules, p1, board);
+        p2_can_play = engine.play(rules, p2, board);
+    }
+
+    int holes = rules.player_holes();
+    for (int i = 0; i < holes; ++i) {
+        if (!p1_can_play) {
+            EXPECT_FALSE(rules.validMove(i, p1, board));
+        }
+        if (!p2_can_play) {
+            EXPECT_FALSE(rules.validMove(i, p2, board));
+        }
+    }
 }
