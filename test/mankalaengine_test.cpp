4afd6a1ed0f49577bd4e68f26ce7509eca3a2658
João Gouveia <joaotalonegouveia@tecnico.ulisboa.pt>
2024-06-10 17:07:20 +0100

    feat: improve tests

--- test/mankalaengine_test.cpp
+++ test/mankalaengine_test.cpp
@@ -3,27 +3,20 @@
 #include <mankalaengine.h>
 #include <moveselection.h>
 
-TEST(MankalaEngine, RandomSelectionFullGame) {
+TEST(MankalaEngine, RandomSelectionFullBohnenspielGame) {
     MankalaEngine::MankalaEngine engine(MankalaEngine::random);
     MankalaEngine::BohnenspielBoard board;
     MankalaEngine::BohnenspielRules rules;
     MankalaEngine::Player p1 = MankalaEngine::player_1;
     MankalaEngine::Player p2 = MankalaEngine::player_2;
-    bool p1_can_play = true;
-    bool p2_can_play = true;
 
-    while (p1_can_play && p2_can_play) {
-        p1_can_play = engine.play(p1, rules, board);
-        p2_can_play = engine.play(p2, rules, board);
+    while (engine.play(p1, rules, board) && engine.play(p2, rules, board)) {
     }
 
     int holes = rules.player_holes();
     for (int i = 0; i < holes; ++i) {
-        if (!p1_can_play) {
-            EXPECT_FALSE(rules.validMove(i, p1, board));
-        }
-        if (!p2_can_play) {
-            EXPECT_FALSE(rules.validMove(i, p2, board));
-        }
+        EXPECT_FALSE(rules.validMove(i, p1, board));
+        EXPECT_FALSE(rules.validMove(i, p2, board));
     }
+    EXPECT_EQ(board.stores.at(p1) + board.stores.at(p2), 6 * 6 * 2);
 }
