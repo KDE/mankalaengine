d252b69ca424c9961c0e4294f219b738afffdee2
João Gouveia <joaotalonegouveia@tecnico.ulisboa.pt>
2024-06-18 13:15:04 +0100

    fix: add spdx header

--- benchmark/benchmark.cpp
+++ benchmark/benchmark.cpp
@@ -1,3 +1,9 @@
+/*
+    SPDX-FileCopyrightText: 2024 João Gouveia <jtalonegouveia@gmail.com>
+
+    SPDX-License-Identifier: GPL-3.0-or-later
+*/
+
 #include <bohnenspielrules.h>
 #include <functional>
 #include <iostream>
