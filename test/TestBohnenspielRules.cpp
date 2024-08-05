54664f3994cef4938121a7af8d3ecd3605e58ca0
João Gouveia <jtalonegouveia@gmail.com>
2024-08-06 00:04:58 +0100

    fix: add spdx file headers

--- test/TestBohnenspielRules.cpp
+++ test/TestBohnenspielRules.cpp
@@ -1,3 +1,9 @@
+/*
+    SPDX-FileCopyrightText: 2024 João Gouveia <jtalonegouveia@gmail.com>
+
+    SPDX-License-Identifier: GPL-3.0-or-later
+*/
+
 #include <QTest>
 #include <bohnenspielrules.h>
 #include <rules.h>
